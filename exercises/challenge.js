332c5f636cb35a4413959dc86b942f5cd4d6d129
bethparnell <beth.parnell@generalassemb.ly>
2022-07-18 14:56:31 -0400

    Update for cohort Ice Pops
    Added solution and tested

--- exercises/challenge.js
+++ exercises/challenge.js
@@ -1,5 +1,5 @@
 /*
-=============== JS Finitee State Machine Challenge ==================
+=============== JS Finite State Machine Challenge ==================
 GOAL: Read each question and write code to complete each task
       given. Do not change starter code or function names.
       
@@ -43,7 +43,56 @@ module.exports = {
 You must use them as parameters to your tapCard and insertTicket methods.
 */
 const SubwayGate = function () {
+  Object.defineProperty(this, '_state', {
+    value: 'closed',
+    configurable: true
+  })
 
+  Object.defineProperty(SubwayGate.prototype, 'state', {
+    enumerable: true,
+    value: function () {
+      return this._state
+    }
+  })
+}
+
+SubwayGate.prototype.exit = function () {
+  Object.defineProperty(this, '_state', {
+    value: 'open'
+  })
+}
+
+SubwayGate.prototype.walkThrough = function () {
+  if (this.state() === 'open') {
+    Object.defineProperty(this, '_state', {
+      value: 'closed'
+    })
+    return true
+  }
+  return false
+}
+
+SubwayGate.prototype.tapCard = function (charlieCard) {
+  if (charlieCard.monthlyValue) {
+    Object.defineProperty(this, '_state', {
+      value: 'open'
+    })
+    return true
+  }
+  return false
+}
+
+SubwayGate.prototype.insertTicket = function (charlieTicket) {
+  if (this.state() === 'closed') {
+    if (charlieTicket.value >= 2.25) {
+      Object.defineProperty(this, '_state', {
+        value: 'open'
+      })
+      charlieTicket.value -= 2.25
+      return true
+    }
+    return false
+  }
 }
 
 
